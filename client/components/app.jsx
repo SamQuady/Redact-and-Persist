6f27bc6ec41894028b4762392bcba3c40204b562
Sam Quady <samquady1@gmail.com>
2021-04-15 18:23:10 -0700

    Added Structure to Main App Component, Routes to Helper Functions

---
+++ client/components/app.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import redacter from '../../helpers/main.js';
+import nonCasedRedacter from '../../helpers/main.js';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      original: '';
+      redacted: '';
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Document Redacter</h2>
+        </div>
+        <div>
+          <div>
+            <h4>Sensitive Words</h4>
+            <input type="text"></input>
+            <button>Enter</button>
+          </div>
+          <div>
+            <h4>Original Document</h4>
+            <input type="text"></input>
+            <button>Enter</button>
+          </div>
+        </div>
+        <div>
+          <button>Redact!</button>
+        </div>
+      </div>
+    )
+  }
+}
